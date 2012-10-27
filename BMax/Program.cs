6cb178f2c7840173694b22cabda8597307d072d4
silverwind <silv3rwind@gmail.com>
2012-10-27 22:59:25 +0300

    New layout

--- BMax/Program.cs
+++ BMax/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading;
-using System.IO;
 using System.Drawing;
 using System.Runtime;
 using System.Runtime.InteropServices;
@@ -24,22 +22,50 @@ namespace BMax {
 			public int Bottom; // y position of lower-right corner
 		}
 		//--------------------------------------------------------------------------------------------
+		public class Window {
+			public IntPtr handle;
+			public string Title;
+			public string WindowClass;
+		}
+
+		public class SavedWindow {
+			public IntPtr handle;
+			public int windowStyle;
+			public RECT windowRect;
+		}
+		//--------------------------------------------------------------------------------------------
+		public const int GCL_HICONSM = -34;
+		public const int GCL_HICON = -14;
+		public const int ICON_SMALL = 0;
+		public const int ICON_BIG = 1;
+		public const int ICON_SMALL2 = 2;
+		public const int WM_GETICON = 0x7F;
+		public Icon GetAppIcon(IntPtr hwnd) {
+			IntPtr iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL2, 0);
+			if( iconHandle == IntPtr.Zero )	iconHandle = SendMessage(hwnd, WM_GETICON, ICON_SMALL, 0);
+			if( iconHandle == IntPtr.Zero )	iconHandle = SendMessage(hwnd, WM_GETICON, ICON_BIG, 0);
+			if( iconHandle == IntPtr.Zero )	iconHandle = GetClassLongPtr(hwnd, GCL_HICON);
+			if( iconHandle == IntPtr.Zero )	iconHandle = GetClassLongPtr(hwnd, GCL_HICONSM);
+			if( iconHandle == IntPtr.Zero )	return null;
+			return Icon.FromHandle(iconHandle);
+		}
+		//--------------------------------------------------------------------------------------------
 		static bool CFG_KEEP_TASKBAR_VISIBLE = true;
 		static int CFG_BORDER_LEFT = 0;
 		static int CFG_BORDER_RIGHT = 0;
 		static int CFG_BORDER_TOP = 0;
 		static int CFG_BORDER_BOTTOM = 0;
-		static bool CFG_USE_STRIP_METHOD = false;
 		//--------------------------------------------------------------------------------------------
 		static int BORDER_LEFT, BORDER_RIGHT, BORDER_TOP, BORDER_BOTTOM;
 		static List<Window> cWindows = new List<Window>();
 		static List<SavedWindow> savedWindows = new List<SavedWindow>();
-
 		static Rectangle bounds = Screen.PrimaryScreen.Bounds;
 		static NotifyIcon trayIcon;
 		static ContextMenuStrip trayMenu;
-		static ToolStripMenuItem windowList, chkTaskbar, switchMethod, strip, resize;
+		static ToolStripMenuItem chkTaskbar;
 		//--------------------------------------------------------------------------------------------
+		delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
 		[DllImport("user32.dll")]
 		static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
@@ -65,6 +91,33 @@ namespace BMax {
 
 		[DllImport("user32.dll", SetLastError = true)]
 		internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+		[DllImport("user32.dll")]
+		static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
+
+		[DllImport("user32.dll")]
+		static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+		[DllImport("user32.dll")]
+		static extern int GetWindowTextLength(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		static extern bool IsWindowVisible(IntPtr hWnd);
+
+		[DllImport("user32.dll")]
+		static extern IntPtr GetShellWindow();
+
+		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+		static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+		[DllImport("user32.dll", EntryPoint = "GetClassLong")]
+		public static extern uint GetClassLongPtr32(IntPtr hWnd, int nIndex);
+
+		[DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
+		public static extern IntPtr GetClassLongPtr64(IntPtr hWnd, int nIndex);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+		static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
 		[Flags()]
 		enum SetWindowPosFlags : uint {
@@ -100,114 +153,104 @@ namespace BMax {
 			ShowDefault = 10,
 			ForceMinimize = 11
 		}
-
 		//--------------------------------------------------------------------------------------------
-		public const int GCL_HICONSM = -34;
-		public const int GCL_HICON = -14;
-
-		public const int ICON_SMALL = 0;
-		public const int ICON_BIG = 1;
-		public const int ICON_SMALL2 = 2;
-
-		public const int WM_GETICON = 0x7F;
-
 		public static IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex) {
 			if( IntPtr.Size > 4 )
 				return GetClassLongPtr64(hWnd, nIndex);
 			else
 				return new IntPtr(GetClassLongPtr32(hWnd, nIndex));
 		}
+		//--------------------------------------------------------------------------------------------
+		public static List<Window> GetActiveWindows() {
+			IntPtr lShellWindow = GetShellWindow();
+			List<Window> windows = new List<Window>();
 
-		[DllImport("user32.dll", EntryPoint = "GetClassLong")]
-		public static extern uint GetClassLongPtr32(IntPtr hWnd, int nIndex);
+			EnumWindows(delegate(IntPtr hWnd, int lParam) {
+				if( hWnd == lShellWindow ) return true;
+				if( !IsWindowVisible(hWnd) ) return true;
 
-		[DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
-		public static extern IntPtr GetClassLongPtr64(IntPtr hWnd, int nIndex);
+				int lLength = GetWindowTextLength(hWnd);
+				if( lLength == 0 ) return true;
 
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-		static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+				StringBuilder WindowTitle = new StringBuilder(lLength);
+				GetWindowText(hWnd, WindowTitle, lLength + 1);
+
+				StringBuilder ClassName = new StringBuilder(256);
+				GetClassName(hWnd, ClassName, ClassName.Capacity);
+
+				Window win = new Window();
+				win.handle = hWnd;
+				win.Title = WindowTitle.ToString();
+				win.WindowClass = ClassName.ToString();
+				windows.Add(win);
+				return true;
+			}, 0);
+			return windows;
+		}
 		//--------------------------------------------------------------------------------------------
 		private BMaxApp() {
-			CreateIconAndMenus();
+			trayMenu = new ContextMenuStrip();
+			trayIcon = new NotifyIcon();
+			trayIcon.MouseClick += trayIcon_Click;
+			trayIcon.Text = "BMax";
+			trayIcon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+			trayIcon.Visible = true;
+			trayIcon.ContextMenuStrip = CreateMenu();
 			CalculateBorders();
 		}
 		//--------------------------------------------------------------------------------------------
-		private void CreateIconAndMenus() {
-			trayMenu = new ContextMenuStrip();
+		private void trayIcon_Click(object sender, EventArgs e) {
+			this.ContextMenuStrip = CreateMenu();
+		}
+		//--------------------------------------------------------------------------------------------
+		private ContextMenuStrip CreateMenu() {
+			trayMenu.Items.Clear();
 
-			windowList = new ToolStripMenuItem("&Toggle Maximize");
-			windowList.DropDownItems.Add("");
-			windowList.DropDown.ItemClicked += new ToolStripItemClickedEventHandler(ToggleMaximized);
-			trayMenu.Items.Add(windowList);
-			trayMenu.Items[0].MouseEnter += new EventHandler(PopulateWindowList);
+			foreach( Window w in GetActiveWindows() ) {
+				var item = new ToolStripMenuItem();
+				item.Text = w.Title.Length < 35 ? w.Title : w.Title.Substring(0, 35) + "...";
+				item.Image = GetAppIcon(w.handle).ToBitmap();
+			//	item.ToolTipText = "Name:\t" + w.Title + "\nClass:\t" + w.WindowClass;
+				item.DropDownItems.Add("Maximize (&Resize)");
+				item.DropDownItems.Add("Maximize (&Strip)");
+				item.DropDownItems.Add("R&estore Window");
+
+				item.DropDownItems[0].Click += (s, a) => Maximize(w.handle, "resize");
+				item.DropDownItems[1].Click += (s, a) => Maximize(w.handle, "strip");
+				item.DropDownItems[2].Click += (s, a) => Maximize(w.handle, "restore");
+				item.DropDownItems[2].Enabled = false;
+				foreach( SavedWindow sw in savedWindows ) {
+					if( sw.handle == w.handle ) {
+						item.DropDownItems[0].Enabled = false;
+						item.DropDownItems[1].Enabled = false;
+						item.DropDownItems[2].Enabled = true;
+					}
+				}
+				trayMenu.Items.Add(item);
+			}
+			trayMenu.Items.Add("-", null);
 
 			chkTaskbar = new ToolStripMenuItem("&Keep Taskbar visible");
 			chkTaskbar.Checked = CFG_KEEP_TASKBAR_VISIBLE;
 			chkTaskbar.CheckOnClick = true;
 			chkTaskbar.CheckedChanged += new EventHandler(chkTaskBar_clicked);
 			trayMenu.Items.Add(chkTaskbar);
-
-			switchMethod = new ToolStripMenuItem("&Switch maximizing method");
-
-			strip = new ToolStripMenuItem("&Strip window");
-			resize = new ToolStripMenuItem("&Resize window");
-
-			strip.Checked = CFG_USE_STRIP_METHOD;
-			resize.Checked = !CFG_USE_STRIP_METHOD;
-
-			strip.Click += new EventHandler(chkStrip_Clicked);
-			resize.Click += new EventHandler(chkResize_Clicked);
-
-			switchMethod.DropDownItems.Add(strip);
-			switchMethod.DropDownItems.Add(resize);
-			trayMenu.Items.Add(switchMethod);
-
 			trayMenu.Items.Add("-", null);
-			trayMenu.Items.Add("&Exit", null);
 
-			trayMenu.Items[4].Click += new EventHandler(Exit);
+			var exit = new ToolStripMenuItem("&Exit");
+			exit.Click += new EventHandler(Exit);
+			trayMenu.Items.Add(exit);
 
-			trayIcon = new NotifyIcon();
-			trayIcon.Text = "BMax";
-			trayIcon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
-			trayIcon.ContextMenuStrip = trayMenu;
-			trayIcon.Visible = true;
+			return trayMenu;
 		}
+
 		//--------------------------------------------------------------------------------------------
-		private void PopulateWindowList(object sender, EventArgs e) {
-			windowList.DropDownItems.Clear();
-			foreach( Window w in GetActiveWindows.Get() ) {
-				windowList.DropDownItems.Add(w.Title);
-				windowList.DropDownItems[windowList.DropDownItems.Count - 1].ToolTipText = w.WindowClass;
-				windowList.DropDownItems[windowList.DropDownItems.Count - 1].Image = GetAppIcon(w.handle).ToBitmap();
-			}
-		}
-
-		private void chkStrip_Clicked(object sender, EventArgs e) {
-			CFG_USE_STRIP_METHOD = true;
-			strip.Checked = CFG_USE_STRIP_METHOD;
-			resize.Checked = !CFG_USE_STRIP_METHOD;
-		}
-
-		private void chkResize_Clicked(object sender, EventArgs e) {
-			CFG_USE_STRIP_METHOD = false;
-			strip.Checked = CFG_USE_STRIP_METHOD;
-			resize.Checked = !CFG_USE_STRIP_METHOD;
-		}
-
 		private void chkTaskBar_clicked(object sender, EventArgs e) {
 			CFG_KEEP_TASKBAR_VISIBLE = chkTaskbar.Checked;
 			CalculateBorders();
 		}
 		//--------------------------------------------------------------------------------------------
-		private void ToggleMaximized(object sender, ToolStripItemClickedEventArgs e) {
-			string wClass = e.ClickedItem.ToolTipText;
-			string wTitle = e.ClickedItem.Text;
-			Maximize((IntPtr)0, wTitle, wClass);
-		}
-		//--------------------------------------------------------------------------------------------
 		private void CalculateBorders() {
-
 			BORDER_LEFT = CFG_BORDER_LEFT;
 			BORDER_RIGHT = CFG_BORDER_RIGHT;
 			BORDER_TOP = CFG_BORDER_TOP;
@@ -234,44 +277,36 @@ namespace BMax {
 			}
 		}
 		//--------------------------------------------------------------------------------------------
-		private static void Maximize(IntPtr wHandle, string wTitle = "", string wClass = "") {
-			if( wTitle != "" && wClass != "" )
-				wHandle = FindWindow(wClass, wTitle);
-
-			bool isMaximized = false;
-			int index = 0;
-			foreach( SavedWindow wd in savedWindows ) {
-				if( wd.handle == wHandle ) {
-					isMaximized = true;
-					break;
+		private static void Maximize(IntPtr wHandle, string action) {
+			if( action == "restore" ) {
+				int index = 0;
+				foreach( SavedWindow sw in savedWindows ) {
+					if( sw.handle == wHandle ) break;
+					++index;
 				}
-				++index;
-			}
 
-			if ( isMaximized )
-			{
-					//restore style
-					SetWindowLong(wHandle, -16, savedWindows[index].windowStyle);
-					//restore position
-					SetWindowPos(wHandle, (IntPtr)(0),
-						savedWindows[index].windowRect.Left,
-						savedWindows[index].windowRect.Top,
-						savedWindows[index].windowRect.Right - savedWindows[index].windowRect.Left,
-						savedWindows[index].windowRect.Bottom - savedWindows[index].windowRect.Top,
-						(SetWindowPosFlags)0x0020);
+				//restore style
+				SetWindowLong(wHandle, -16, savedWindows[index].windowStyle);
+				//restore position
+				SetWindowPos(wHandle, (IntPtr)(0),
+					savedWindows[index].windowRect.Left,
+					savedWindows[index].windowRect.Top,
+					savedWindows[index].windowRect.Right - savedWindows[index].windowRect.Left,
+					savedWindows[index].windowRect.Bottom - savedWindows[index].windowRect.Top,
+					(SetWindowPosFlags)0x0020);
 
-					ShowWindow(wHandle, (ShowWindowCommands)5);
+				ShowWindow(wHandle, (ShowWindowCommands)5);
 
-					MoveWindow(wHandle,
-						savedWindows[index].windowRect.Left,
-						savedWindows[index].windowRect.Top,
-						savedWindows[index].windowRect.Right - savedWindows[index].windowRect.Left,
-						savedWindows[index].windowRect.Bottom - savedWindows[index].windowRect.Top,
-						true);
-					savedWindows.RemoveAt(index);
+				MoveWindow(wHandle,
+					savedWindows[index].windowRect.Left,
+					savedWindows[index].windowRect.Top,
+					savedWindows[index].windowRect.Right - savedWindows[index].windowRect.Left,
+					savedWindows[index].windowRect.Bottom - savedWindows[index].windowRect.Top,
+					true);
+				savedWindows.RemoveAt(index);
 
 			} else {
-				//Save data
+				//save position and style for restoring
 				int wStyle = GetWindowLong(wHandle, -16);
 				RECT rWind;
 				GetWindowRect(wHandle, out rWind);
@@ -282,9 +317,7 @@ namespace BMax {
 				w.windowStyle = wStyle;
 				savedWindows.Add(w);
 
-				if( CFG_USE_STRIP_METHOD ) {
-				//Strip Method
-					//SetWindowLong(wHandle, -16, wStyle & ~(0x00040000 | 0x00C00000));
+				if( action == "strip" ) {
 					SetWindowLong(wHandle, -16, wStyle & ~(0x00040000 | 0x00C00000));
 
 					int LEFT = bounds.Left + BORDER_LEFT;
@@ -292,11 +325,9 @@ namespace BMax {
 					int RIGHT = bounds.Right - BORDER_RIGHT - BORDER_LEFT;
 					int BOTTOM = bounds.Bottom - BORDER_BOTTOM - BORDER_TOP;
 
-					SetWindowPos(wHandle, (IntPtr)(0),	LEFT, TOP, RIGHT, BOTTOM, (SetWindowPosFlags)0x0020);
-
+					SetWindowPos(wHandle, (IntPtr)(0), LEFT, TOP, RIGHT, BOTTOM, (SetWindowPosFlags)0x0020);
 					ShowWindow(wHandle, (ShowWindowCommands)5);
-				} else {
-				//Resize Method
+				} else if (action == "resize") {
 					RECT rClient;
 					GetWindowRect(wHandle, out rWind);
 					GetClientRect(wHandle, out rClient);
@@ -308,7 +339,9 @@ namespace BMax {
 					int RIGHT = bounds.Right + (border_thickness * 2);
 					int BOTTOM = bounds.Bottom - BORDER_BOTTOM + titlebar_height + border_thickness;
 
+					ShowWindow(wHandle, (ShowWindowCommands)9);
 					MoveWindow(wHandle, LEFT, TOP, RIGHT, BOTTOM, true);
+					ShowWindow(wHandle, (ShowWindowCommands)5);
 				}
 			}
 		}
@@ -320,98 +353,12 @@ namespace BMax {
 		}
 		//--------------------------------------------------------------------------------------------
 		protected override void Dispose(bool isDisposing) {
-			if( isDisposing )
-				trayIcon.Dispose();
+			if( isDisposing ) trayIcon.Dispose();
 			base.Dispose(isDisposing);
 		}
 		//--------------------------------------------------------------------------------------------
 		private void Exit(object sender, EventArgs e) {
 			Application.Exit();
-		}
-		//--------------------------------------------------------------------------------------------
-		public class Window {
-			public IntPtr handle;
-			public string Title;
-			public string WindowClass;
-		}
-
-		public class SavedWindow {
-			public IntPtr handle;
-			public int windowStyle;
-			public RECT windowRect;
-		}
-
-		//--------------------------------------------------------------------------------------------
-		public Icon GetAppIcon(IntPtr hwnd)
-		{
-		  IntPtr iconHandle = SendMessage(hwnd,WM_GETICON,ICON_SMALL2,0);
-		  if(iconHandle == IntPtr.Zero)
-		    iconHandle = SendMessage(hwnd,WM_GETICON,ICON_SMALL,0);
-		  if(iconHandle == IntPtr.Zero)
-		    iconHandle = SendMessage(hwnd,WM_GETICON,ICON_BIG,0);
-		  if (iconHandle == IntPtr.Zero)
-		    iconHandle = GetClassLongPtr(hwnd, GCL_HICON);
-		  if (iconHandle == IntPtr.Zero)
-		    iconHandle = GetClassLongPtr(hwnd, GCL_HICONSM);
-
-		  if(iconHandle == IntPtr.Zero)
-		    return null;
-
-		  Icon icn = Icon.FromHandle(iconHandle);
-
-		  return icn;
-		}
-		//--------------------------------------------------------------------------------------------
-		public static class GetActiveWindows {
-			delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
-
-			[DllImport("user32.dll")]
-			static extern bool EnumWindows(EnumWindowsProc enumFunc, int lParam);
-
-			[DllImport("user32.dll")]
-			static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
-			[DllImport("user32.dll")]
-			static extern int GetWindowTextLength(IntPtr hWnd);
-
-			[DllImport("user32.dll")]
-			static extern bool IsWindowVisible(IntPtr hWnd);
-
-			[DllImport("user32.dll")]
-			static extern IntPtr GetShellWindow();
-
-			[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-			static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-			public static List<Window> Get() {
-				IntPtr lShellWindow = GetShellWindow();
-				List<Window> windows = new List<Window>();
-
-				EnumWindows(delegate(IntPtr hWnd, int lParam) {
-					if( hWnd == lShellWindow )
-						return true;
-					if( !IsWindowVisible(hWnd) )
-						return true;
-
-					int lLength = GetWindowTextLength(hWnd);
-					if( lLength == 0 )
-						return true;
-
-					StringBuilder WindowTitle = new StringBuilder(lLength);
-					GetWindowText(hWnd, WindowTitle, lLength + 1);
-
-					StringBuilder ClassName = new StringBuilder(256);
-					GetClassName(hWnd, ClassName, ClassName.Capacity);
-
-					Window win = new Window();
-					win.handle = hWnd;
-					win.Title = WindowTitle.ToString();
-					win.WindowClass = ClassName.ToString();
-					windows.Add(win);
-					return true;
-				}, 0);
-				return windows;
-			}
 		}
 	}
 }
